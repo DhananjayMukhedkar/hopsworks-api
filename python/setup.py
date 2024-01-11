538fad23491fe24493fa31254a0c3d33819a7c97
DhananjayMukhedkar <dhananjay1098@gmail.com>
2024-01-11 15:32:05 +0100

    test similarity search

--- python/setup.py
+++ python/setup.py
@@ -18,7 +18,7 @@ setup(
     name="hopsworks",
     version=__version__,
     install_requires=[
-        "hsfs @ git+https://git@github.com/DhananjayMukhedkar/feature-store-api@aiomy#egg=hsfs[python]&subdirectory=python",
+        "hsfs @ git+https://git@github.com/DhananjayMukhedkar/feature-store-api@fix_sim_search#egg=hsfs[python]&subdirectory=python",
         "hsml @ git+https://git@github.com/logicalclocks/machine-learning-api@main#egg=hsml&subdirectory=python",
         "pyhumps==1.6.1",
         "requests",
